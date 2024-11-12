f715eef2829d79a5c56d0a40f45b1399fd48212b
Yoon Jaegyeong <endercraft09@naver.com>
2024-11-13 01:27:40 +0900

    chore: Interactionmanager에서 TwoDScene 생성 / 초기화
    Interactionmanager에서 수집한 씬 내 haircomponent의 정보를 TwoDScene을 생성해 저장. 기존 xmlParser 클래스의 기능과 유사함

--- Assets/Interface/Script/InteractionManager.cs
+++ Assets/Interface/Script/InteractionManager.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InteractionManager : MonoBehaviour
@@ -7,23 +10,49 @@ public class InteractionManager : MonoBehaviour
     private List<LiquidComponent> LiquidComponents;
 
     private List<HairComponent> HairComponents;
-    private bool IsAnySimulComponent = false;
+
+    private List<StrandParameters> allStrandParameters;
+
+    private TwoDScene libwethairScene;
+    [SerializeField]
+    private WetHairParameter SimulationParameter;
+
+    [SerializeField]
+    private bool isMassSpring = false;
+
+    private bool IsFluidSimul = false;
+    private bool IsHairSimul = false;
     // Start is called before the first frame update
 
     private void Awake()
     {
-        GameObject[] Liquids = GameObject.FindGameObjectsWithTag("Liquid"); // need Assertion Check
-        foreach ( GameObject obj in Liquids)
-        {
-            LiquidComponents.Add(GetComponent<LiquidComponent>());
-        }
-        GameObject[] Hairs = GameObject.FindGameObjectsWithTag("Hair");
-        foreach (GameObject obj in Hairs)
-        {
-            HairComponents.Add(GetComponent<HairComponent>());
-        }
-        if (Liquids.Length > 0 && Hairs.Length > 0) IsAnySimulComponent = true;
 
+        LiquidComponents = new List<LiquidComponent>();
+        HairComponents = new List<HairComponent>();
+        allStrandParameters = new List<StrandParameters>();
+        var Liquids = FindObjectsOfType(typeof(LiquidComponent)); // need Assertion Check
+        
+        foreach (var obj in Liquids)
+        {
+            Debug.Log(obj);
+            LiquidComponents.Add(obj.GetComponent<LiquidComponent>());
+        }
+        if (Liquids.Length > 0) IsFluidSimul = true;
+
+        var Hairs = FindObjectsOfType(typeof(HairComponent));
+        foreach (var obj in Hairs)
+        {
+            Debug.Log(obj.name);
+            HairComponent tempComp = obj.GetComponent<HairComponent>();
+            HairComponents.Add(tempComp);
+            foreach (var param in tempComp.GetStrandHairParameters()) {
+                allStrandParameters.Add(param);
+            }
+            
+        }
+        if (Hairs.Length > 0) IsHairSimul=true;
+
+        InitSimulScene();
     }
     void Start()
     {
@@ -34,4 +63,98 @@ public class InteractionManager : MonoBehaviour
     {
         
     }
+
+    private void InitSimulScene() //Awake()에서 호출, haircomponents에서 m, v, fixed 추출
+    {
+        libwethairScene = new TwoDScene(isMassSpring);
+
+        if (IsFluidSimul) { }
+
+        Debug.Log(IsHairSimul);
+        if (IsHairSimul)
+        {
+
+            int numparticles = 0;
+            int numstrands = 0;
+
+            for (int i = 0; i < HairComponents.Count; i++)
+            {
+
+                numparticles += HairComponents[i].hairParticles.Count;
+                numstrands += HairComponents[i].numofStrands;
+
+            }
+            List<bool> tipVerts = new List<bool>(numparticles);
+            int[] strandEnd = new int[numstrands];
+            int strandCount = 0;
+            for (int i = 0; i < HairComponents.Count; i++)
+            {
+                for (int j = 0; j < HairComponents[i].StartofEachParam.Length; j++)
+                {
+                    if (strandCount == 0)
+                    {
+                        strandEnd[strandCount] =  HairComponents[i].EndofEachParam[j] + HairComponents[i].StartofEachParam[j];
+                    }
+                    else
+                    {
+                        strandEnd[strandCount] = strandEnd[strandCount - 1] + 1 + HairComponents[i].EndofEachParam[j] + HairComponents[i].StartofEachParam[j];
+                    }
+                    strandCount++;
+                }
+            }
+
+            libwethairScene.resizeSystem
+                (numparticles,
+                numparticles - 1,
+                numstrands);
+
+            List<int> vert_to_dof = new List<int>();
+            VectorXi dofVars = new VectorXi(numparticles * 4 - numstrands + 1);
+            VectorXi dofVerts = new VectorXi(numparticles * 4 - numstrands + 1);
+            VectorXi dofs = new VectorXi(4);
+
+            
+            for (int k = 0; k < 4; k++)
+            {
+                dofs[k] = k;
+            }
+
+            int dof = 0;
+            strandCount = 0;
+            for (int i = 0; i < numparticles; i++)
+            {
+                dofVars.SetSegment(dof,4, dofs);
+                dofVerts.SetSegment(dof, 4, dofVerts.segment(dof, 4).setConstant(numparticles));
+                vert_to_dof.Add(dof);
+                dof += 4;
+                if (i == strandEnd[strandCount]) 
+                {
+                    dof-- ; 
+                    strandCount++;
+                    tipVerts[i] = true;
+                }
+            }
+            dofVars.conservativeResize(numparticles * 4 - numstrands);
+            dofVerts.conservativeResize(numparticles * 4 - numstrands);
+            libwethairScene.setVertToDoFMap(vert_to_dof, dofVars, tipVerts, dofVerts);
+
+            int partNum = 0;
+            for (int i = 0; i < HairComponents.Count; i++)
+            {
+                for (int j = 0; j < HairComponents[i].m_hair_x.Length; j++)
+                {
+                    libwethairScene.setPosition(partNum, HairComponents[i].m_hair_x[j]);
+                    libwethairScene.setVelocity(partNum, HairComponents[i].m_hair_v[j]);
+                    libwethairScene.setFixed(partNum, HairComponents[i].m_hair_fixed[j]);
+                    partNum++;
+                }
+                
+            }
+                
+        }
+        for (int i = 0; i < (libwethairScene.getX().size() / 3); i++)
+            Debug.Log("X position of " + i + "th particle is " + libwethairScene.getPosition(i)[0]);
+        Debug.Log("Completed!");
+    }
+   
 }
